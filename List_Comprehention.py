a5e7ace9031a82a0b665afc0295cc6511959623c
naveen701526 <70085321+naveen701526@users.noreply.github.com>
2020-10-01 14:13:30 +0530

    Update List_Comprehention.py

--- List_Comprehention.py
+++ List_Comprehention.py
@@ -49,9 +49,6 @@
 
 
 if __name__ == '__main__':
-    x = int(raw_input())
-    y = int(raw_input())
-    z = int(raw_input())
-    n = int(raw_input())
+    x,y,z,n = [int(x) for x in input().split()]
 
 print[[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if((i+j+k) != n)]
